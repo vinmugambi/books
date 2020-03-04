5ca8104e3bd918856d7513231bf74ef8600cb67d
vinmugambi <vinmugambi17@gmail.com>
2020-03-04 17:30:47 +0300

    make Book name fillbale

--- app/Category.php
+++ app/Category.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $guard = [];
+    protected $fillable = ["name"];
 
     public function books(){
         return $this->hasMany(Book::class);
