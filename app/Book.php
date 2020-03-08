51db3658564d8dd63abb832e739403c3ec303efc
vinmugambi <vinmugambi17@gmail.com>
2020-03-08 15:05:04 +0300

    featured not updating as expected

--- app/Book.php
+++ app/Book.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Book extends Model
 {
     protected $guard = [];
-    protected $fillable = ["title","author"];
+    protected $fillable = ["title","author","featured"];
 
     public function category(): BelongsTo {
        return $this->belongsTo(Category::class);
