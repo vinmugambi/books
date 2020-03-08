0a9085a678ec9d90b62b3b40d2ec36e547209324
vinmugambi <vinmugambi17@gmail.com>
2020-03-08 13:19:49 +0300

    add graphql server operations for book

--- app/Book.php
+++ app/Book.php
@@ -3,12 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
     protected $guard = [];
+    protected $fillable = ["title","author"];
 
-    public function category(){
+    public function category(): BelongsTo {
        return $this->belongsTo(Category::class);
     }
 }
