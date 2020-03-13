3bde49bd9634a7a3248e046940522b017b2def0f
vinmugambi <vinmugambi17@gmail.com>
2020-03-13 05:03:11 +0300

    featured category error

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -29,7 +29,7 @@
                 class="link-margin block"
               >{{book.id}}. {{ book.title }}</router-link>
               <div>{{book.author}}</div>
-              <img :src="book.image" alt="cover-image">
+              <img :src="book.image" alt="cover-image" />
             </div>
           </div>
         </template>
@@ -47,27 +47,30 @@
                 class="link-margin block"
               >{{book.id}}. {{ book.title }}</router-link>
               <div>{{book.author}}</div>
-              <img :src="book.image" alt="cover-image">
+              <img :src="book.image" alt="cover-image" />
             </div>
           </div>
         </template>
       </ApolloQuery>
     </div>
     <!-- Books in the selected category -->
-    <div v-else>
-      <ApolloQuery :query="query" :variables="{id: selectedCategory}">
-        <template slot-scope="{ result: { data, loading },isLoading }">
+    <div>
+      <ApolloQuery :query="query" :variables="{id: Number(selectedCategory)}">
+        <template slot-scope="{ result: { data, error },isLoading }">
           <div v-if="isLoading">Loading...</div>
-          <div v-else>
-            <div v-for="book of data.category.books" :key="book.id">
+          <div v-else-if="error" class="error apollo">An error occurred</div>
+          <div v-else-if="data">
+            <!-- <div v-for="book of data.category.books" :key="book.id">
               <router-link
                 :to="`/books/${book.id}`"
                 class="link-margin block"
               >{{book.id}}. {{ book.title }}</router-link>
               <div>{{book.author}}</div>
-              <img :src="book.image" alt="cover-image">
-            </div>
+              <img :src="book.image" alt="cover-image" />
+            </div> -->
+            <pre>{{data}}</pre>
           </div>
+          <div v-else class="no-result apollo">No result :(</div>
         </template>
       </ApolloQuery>
     </div>
